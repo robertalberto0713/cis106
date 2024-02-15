7685873a4d417d55794b81c7b56ba0db5d90dae6
robertalberto0713 <robert.alberto0713@gmail.com>
2024-02-15 02:13:50 +0000

    lab 3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "The current date is:"
+date -R
+echo " "
+echo "Some information about my system: "
+uname -a
+echo " "
+echo "RAM Information"
+free 
+echo " "
+echo "Challenge Question Complete "
