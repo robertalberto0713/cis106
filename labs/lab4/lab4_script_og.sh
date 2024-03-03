13a554301da9c8f132b0e94bfffbb02c423bb62b
robertalberto0713 <robert.alberto0713@gmail.com>
2024-03-03 16:52:27 +0000

    lab4 wr4 notes 4 done

---
+++ labs/lab4/lab4_script_og.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+down="$HOME/Downloads"
+cis="$HOME/cis106"
+msg1="The current directory is:"
+msg2="\nAll files (including hidden files) in"
+
+echo "The current user is $USER. It's home directory is $HOME"
+cd $down
+echo $msg1
+pwd
+cd $cis
+echo $msg1
+pwd
+
+echo -e "$msg2 /"
+ls -mA --color=auto /
+
+echo -e "$msg2 $HOME"
+ls -mA --color=auto $HOME
+
+echo -e "$msg2 $down"
+ls -mA --color=auto $down
+
+echo -e "$msg2 $cis"
+ls -mA --color=auto $cis
+
+
