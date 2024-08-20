1b17bcfa7c673fa8f14a89036c21c1274cbedb13
Robert alberto <155783370+robertalberto0713@users.noreply.github.com>
2024-08-19 22:02:59 -0400

    Delete labs directory

--- labs/lab3/challenge-lab3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo -e "\e[35m“In the middle of the journey of our life I found myself within a dark woods where the straight way was lost.”\e[0m"
-echo -e "\e[1m \e[31m \t― Dante Alighieri, Inferno\e[0m"
